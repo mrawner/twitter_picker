fc5ca1e0e951263d3e1c9d31a3e2b5bdd75963a0
Michael Rawner <82424025+mrawner@users.noreply.github.com>
2021-12-10 19:26:10 -0300

    Update winner_picker.py
    Adding the non participants option.

--- winner_picker.py
+++ winner_picker.py
@@ -1,14 +1,24 @@
 import random as rand
 
-participants = int(input('Select the number of participants: '))
+participants = int(input('Enter the number of participants: '))
 max_winners = int(input('Select # of winners: '))
+
+not_participate = []
+n = int(input("Select # of non participants: "))
+
+for i in range(0, n):
+    nonpart = int(input('Enter each NON participants: '))
+ 
+    not_participate.append(nonpart)
+     
+print('Non participants are ',not_participate)
 
 if max_winners <= participants:
     winners = []
     while len(winners) < max_winners:
         pick = rand.randint(1,participants)
-        if pick not in winners:
+        if (pick not in winners) and (pick not in not_participate):
             winners.append(pick)
-    print('The winners are: ', winners)
+    print('The winner/s is/are: ', winners)
 else:
     pass
